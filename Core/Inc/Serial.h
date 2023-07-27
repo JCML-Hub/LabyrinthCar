f9464968c41d3ead965b9c93fe927ce2ea13afbb
芜湖 <123913114+JCML-Hub@users.noreply.github.com>
2023-07-27 13:05:02 +0800

    完善多通道串口的收发，另外一个通道还未测试

--- Core/Inc/Serial.h
+++ Core/Inc/Serial.h
@@ -5,32 +5,40 @@
 
 #define MAX_RX_LEN (256U) // 一次性可以接受的数据字节长度，你可以自己定义。U是Unsigned的意思。
 #define MAX_TX_LEN (512U) // 一次性可以发送的数据字节长度，你可以自己定义。
+
 #include "stdio.h"
 #include "string.h"
 #include <stdarg.h>
+#include "usart.h"
 
-#define UARTA huart1
+/*使能串口，同时定义其接口*/
+#define ENABLEUARTA huart1
+//#define ENABLEUARTB huart2
 
+#ifdef ENABLEUARTA
+extern char RxDataStr[MAX_RX_LEN];
 
 extern DMA_HandleTypeDef hdma_usart1_rx;
 extern DMA_HandleTypeDef hdma_usart1_tx;
+#define UART_DMA_CH1_Rx hdma_usart1_rx
+#define UART_DMA_CH1_Tx hdma_usart1_rx
 
+#endif
 
-#ifdef ENABLEUART2
+#ifdef ENABLEUARTB
+char RxDataStr_Ch2[MAX_RX_LEN];
+
 extern DMA_HandleTypeDef hdma_usart2_rx;
 extern DMA_HandleTypeDef hdma_usart2_tx;
+#define UART_DMA_CH2_Rx hdma_usart2_rx
+#define UART_DMA_CH2_Tx hdma_usart2_rx
 #endif
-extern uint8_t *RxData;
-extern uint8_t *p_IsToReceive;
-extern uint8_t WhichBufIsReady;
-extern char RxDataStr[MAX_RX_LEN];
 
-
-void USART1_Init(void);
-void USART2_Init(void);
-void MyPrintf(const char *__format, ...);
-_Bool GetRxFlag(void);
+void USART_Init(UART_HandleTypeDef *huart);
+void MyPrintf(const char *format,...);//默认以 ENABLEUARTA 通信
+void MyPrintf_MIX(UART_HandleTypeDef *huart,const char *format,...);
+_Bool GetRxFlag(UART_HandleTypeDef *huart);
 void StringTrans(void);
 float GetRxValue(char *Str, uint8_t lenth);
-void USART1_RX_CallBack(void);
+void USART_RX_CallBack(UART_HandleTypeDef *huart);//需要将其置于 "stm32f1xx_it.h" 中
 #endif
--- Core/Src/Serial.c
+++ Core/Src/Serial.c
@@ -13,7 +13,6 @@
  */
 #include <stdlib.h>
 #include "main.h"
-#include "usart.h"
 #include "Serial.h"
 
 uint8_t  TxBuf[MAX_TX_LEN];  /* 发送缓冲区 */
@@ -29,6 +28,18 @@ char RxDataStr[MAX_RX_LEN];
 uint8_t Rxflag;
 uint32_t data_length;
 
+#ifdef ENABLEUARTB
+uint8_t WhichBufIsReady_CH2 = 0;         // 双缓存指示器。
+uint8_t u1rxbuf_CH2[MAX_RX_LEN];         // 数据接收缓冲1
+uint8_t u2rxbuf_CH2[MAX_RX_LEN];         // 数据接收缓冲2
+uint8_t *RxData_CH2 = u2rxbuf;        // 指针——指向可以读取的那个缓冲
+uint8_t *p_IsToReceive_CH2 = u1rxbuf; // 指针——指向被占用的那个缓冲
+
+char RxDataStr_Ch2[MAX_RX_LEN];
+uint8_t Rxflag_Ch2;
+uint32_t data_length_Ch2;
+#endif
+
 
 /***********************************************************
 *@fuction	:USART1_Init
@@ -39,33 +50,32 @@ uint32_t data_length;
 *@date		:2023-01-17
 ***********************************************************/
 
-void USART1_Init(void)
+void USART_Init(UART_HandleTypeDef *huart)
 {
-  // 开启串口1空 闲中断
-  __HAL_UART_ENABLE_IT(&huart1, UART_IT_IDLE);
+  // 开启串口空 闲中断
+  __HAL_UART_ENABLE_IT(huart, UART_IT_IDLE);
   // 开启DMA发送通道的发送完成中断，才能实现封装发送函数里面的等待功能
-  __HAL_DMA_ENABLE_IT(&hdma_usart1_tx, DMA_IT_TC);
-  // 清除空闲标志位，防止中断误入
-  __HAL_UART_CLEAR_IDLEFLAG(&huart1);
-  // 立即就要打开DMA接收
-  // 不然DMA没有提前准备，第一次接收的数据是读取不出来的
-  HAL_UART_Receive_DMA(&huart1, p_IsToReceive, MAX_RX_LEN);
-}
-
-#ifdef ENABLEUART2
-void USART2_Init(void)
-{
-  // 开启串口1空 闲中断
-  __HAL_UART_ENABLE_IT(&huart2, UART_IT_IDLE);
-  // 开启DMA发送通道的发送完成中断，才能实现封装发送函数里面的等待功能
-  __HAL_DMA_ENABLE_IT(&hdma_usart2_tx, DMA_IT_TC);
-  // 清除空闲标志位，防止中断误入
-  __HAL_UART_CLEAR_IDLEFLAG(&huart2);
-  // 立即就要打开DMA接收
-  // 不然DMA没有提前准备，第一次接收的数据是读取不出来的
-  HAL_UART_Receive_DMA(&huart2, p_IsToReceive, MAX_RX_LEN);
-}
+  if (huart==&ENABLEUARTA) {
+    __HAL_DMA_ENABLE_IT(&UART_DMA_CH1_Tx, DMA_IT_TC);
+  }
+#ifdef ENABLEUARTB
+  if (huart==&ENABLEUARTB){
+    __HAL_DMA_ENABLE_IT(&UART_DMA_CH2_Tx, DMA_IT_TC);
+  }
 #endif
+  // 清除空闲标志位，防止中断误入
+  __HAL_UART_CLEAR_IDLEFLAG(huart);
+  // 立即就要打开DMA接收
+  // 不然DMA没有提前准备，第一次接收的数据是读取不出来的
+  if (huart==&ENABLEUARTA) {
+    HAL_UART_Receive_DMA(huart, p_IsToReceive, MAX_RX_LEN);//开启接收
+  }
+#ifdef ENABLEUARTB
+  if (huart==&ENABLEUARTB){
+    HAL_UART_Receive_DMA(huart, p_IsToReceive_CH2, MAX_RX_LEN);
+  }
+#endif
+}
 
 /***********************************************************
 *@fuction	:StringTrans
@@ -88,8 +98,24 @@ void StringTrans(void)
 		else {RxDataStr[i]=RxData[i];}
 		i++;
 	}
-	RxDataStr[data_length]='\0';
+	RxDataStr[data_length-1]='\0';
 }
+#ifdef ENABLEUARTB
+void StringTrans_CH2(void)
+{
+  uint32_t i=0;
+  while (i < data_length_Ch2)
+  {
+    if (RxData_CH2[i] == '#' ||RxData_CH2[i] == 0)
+    {//检验是否以规定的通信方式传输
+      break;
+    }
+    else {RxDataStr_Ch2[i]=RxData_CH2[i];}
+    i++;
+  }
+  RxDataStr_Ch2[data_length_Ch2]='\0';
+}
+#endif
 /***********************************************************
 *@名称 	:GetRxValue
 *@描述	:转换获取从串口中接收数字 输出为float型
@@ -128,10 +154,16 @@ float GetRxValue(char *Str, uint8_t lenth)
 *@date		:2023-07-26
 ***********************************************************/
 
-_Bool GetRxFlag(void)
+_Bool GetRxFlag(UART_HandleTypeDef *huart)
 {
-	if (Rxflag==1){StringTrans();Rxflag=0;return 1;}
-	else {return 0;}
+  if (huart==&ENABLEUARTA && Rxflag==1){
+    StringTrans();Rxflag=0;return 1;
+  }
+#ifdef ENABLEUARTB
+  else if (huart==&ENABLEUARTB && Rxflag_Ch2==1){
+    StringTrans_CH2();Rxflag_Ch2=0;return 1;}
+#endif
+  else {return 0;}
 }
 /***********************************************************
 *@fuction	:MyPrintf
@@ -141,7 +173,22 @@ _Bool GetRxFlag(void)
 *@author	:JCML
 *@date		:2023-07-26
 ***********************************************************/
-void MyPrintf(const char *format, ...)//加载到DMA中发送
+void MyPrintf(const char *format,...)//加载到DMA中发送//默认通道
+{
+  uint32_t length;
+  va_list args;
+  /* 清空发送缓冲区 */
+  memset(TxBuf, 0, TX_BUF_LEN);
+  /* 填充发送缓冲区 */
+  va_start(args, format);
+  length = vsnprintf((char *)TxBuf, sizeof(TxBuf), format, args);
+  va_end(args);
+  /* 往串口发送数据 */
+  HAL_UART_Transmit_DMA(&ENABLEUARTA, (uint8_t*)&TxBuf, length);
+  while(__HAL_UART_GET_FLAG(&ENABLEUARTA, UART_FLAG_TC) == RESET){}//如果数据未发完就将其卡在此处
+}
+/*多通道的*/
+void MyPrintf_MIX(UART_HandleTypeDef *huart,const char *format,...)//加载到DMA中发送
 {
   uint32_t length;
   va_list args;
@@ -152,30 +199,26 @@ void MyPrintf(const char *format, ...)//加载到DMA中发送
   length = vsnprintf((char *)TxBuf, sizeof(TxBuf), (char *)format, args);
   va_end(args);
   /* 往串口发送数据 */
-  HAL_UART_Transmit_DMA(&UARTA, (uint8_t*)&TxBuf, length);
+  HAL_UART_Transmit_DMA(huart, (uint8_t*)&TxBuf, length);
+  while(__HAL_UART_GET_FLAG(huart, UART_FLAG_TC) == RESET){}
 }
 
 /***********************************************************
 *@fuction	:USART1_IRQHandler
-*@brief		:串口接收数据中断处理
+*@brief		:串口接收的回调函数
 *@param		:void
 *@return	:void
 *@author	:JCML
 *@date		:2023-07-26
 ***********************************************************/
 
-void USART1_RX_CallBack(void)
+void USART_RX_CallBack(UART_HandleTypeDef *huart)
 {
-  /* USER CODE BEGIN USART1_IRQn 0 */
-  if (RESET != __HAL_UART_GET_FLAG(&huart1, UART_FLAG_IDLE))
+  if (RESET != __HAL_UART_GET_FLAG(&ENABLEUARTA, UART_FLAG_IDLE))
   {
     Rxflag = 1;
-    // __HAL_UART_CLEAR_IDLEFLAG(&huart1);
-    // 这一部分其实在 HAL_UART_IRQHandler(&huart1) 也完成了。
-
-    HAL_UART_DMAStop(&huart1); // 把DMA接收停掉，防止速度过快导致中断重入，数据被覆写。
-
-    data_length = MAX_RX_LEN - __HAL_DMA_GET_COUNTER(&hdma_usart1_rx);
+    HAL_UART_DMAStop(&ENABLEUARTA); // 把DMA接收停掉，防止速度过快导致中断重入，数据被覆写。
+    data_length = MAX_RX_LEN - __HAL_DMA_GET_COUNTER(&UART_DMA_CH1_Rx);
     // 数据总长度=极限接收长度-DMA剩余的接收长度
     if (WhichBufIsReady)	//WhichBufIsReady=1
     {
@@ -190,11 +233,36 @@ void USART1_RX_CallBack(void)
       WhichBufIsReady = 1;		//切换一下指示器状态
     }
     //从下面开始可以处理你接收到的数据啦！举个栗子，把你收到的数据原原本本的还回去
-
-    //DMA_USART1_Tx_Data(RxData,data_length);//数据打回去，长度就是数据长度
-
+//    HAL_UART_Transmit_DMA(&ENABLEUARTA,RxData,data_length);//数据打回去，长度就是数据长度
     memset((uint8_t *)p_IsToReceive, 0, MAX_RX_LEN);	// 把接收数据的指针指向的缓冲区清空
   }
-  HAL_UART_Receive_DMA(&huart1, p_IsToReceive, MAX_RX_LEN); //数据处理完毕，重新启动接收
+  HAL_UART_Receive_DMA(&ENABLEUARTA, p_IsToReceive, MAX_RX_LEN); //数据处理完毕，重新启动接收
+
+
+#ifdef ENABLEUARTB
+  if (RESET != __HAL_UART_GET_FLAG(&ENABLEUARTB, UART_FLAG_IDLE))
+  {
+    Rxflag_Ch2 = 1;
+    HAL_UART_DMAStop(&ENABLEUARTB); // 把DMA接收停掉，防止速度过快导致中断重入，数据被覆写。
+    data_length_Ch2 = MAX_RX_LEN - __HAL_DMA_GET_COUNTER(&UART_DMA_CH1_Rx);
+    // 数据总长度=极限接收长度-DMA剩余的接收长度
+    if (WhichBufIsReady_CH2)	//WhichBufIsReady=1
+    {
+      RxData_CH2 = u2rxbuf_CH2;        // u2rxbuf 可以读取，就绪指针指向它。
+      p_IsToReceive_CH2 = u1rxbuf_CH2; // u1rxbuf 作为下一次DMA存储的缓冲，占用指针指向它。
+      WhichBufIsReady_CH2 = 0;		//切换一下指示器状态
+    }
+    else				//WhichBufIsReady=0
+    {
+      RxData_CH2 = u1rxbuf_CH2;        // u1rxbuf 可以读取，就绪指针指向它。
+      p_IsToReceive_CH2 = u2rxbuf_CH2; // u2rxbuf 作为下一次DMA存储的缓冲，占用指针指向它。
+      WhichBufIsReady_CH2= 1;		//切换一下指示器状态
+    }
+    //从下面开始可以处理你接收到的数据啦！举个栗子，把你收到的数据原原本本的还回去
+//    HAL_UART_Transmit_DMA(&ENABLEUARTB,RxData,data_length);//数据打回去，长度就是数据长度
+    memset((uint8_t *)p_IsToReceive_CH2, 0, MAX_RX_LEN);	// 把接收数据的指针指向的缓冲区清空
+  }
+  HAL_UART_Receive_DMA(&ENABLEUARTB, p_IsToReceive_CH2, MAX_RX_LEN); //数据处理完毕，重新启动接收
+#endif
 }
 
